eedc6ca440b7a7f6ef6c01d2dd67ba2cc4932b07
bartick <bartickmaiti1998@gmail.com>
2023-04-14 00:19:59 +0530

    config: firebase keys added

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || '3000';
+export const FIREBASE_APIKEY = process.env.FIREBASE_APIKEY || '';
+export const FIREBASE_AUTHDOMAIN = process.env.FIREBASE_AUTHDOMAIN || '';
+export const FIREBASE_PROJECTID = process.env.FIREBASE_PROJECTID || '';
+export const FIREBASE_STORAGEBUCKET = process.env.FIREBASE_STORAGEBUCKET || '';
+export const FIREBASE_MESSAGINGSENDERID = process.env.FIREBASE_MESSAGINGSENDERID || '';
+export const FIREBASE_APPID = process.env.FIREBASE_APPID || '';
+export const FIREBASE_MEASUREMENTID = process.env.FIREBASE_MEASUREMENTID || '';
